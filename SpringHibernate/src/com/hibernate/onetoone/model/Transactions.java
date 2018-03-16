b9600e76503ff952c83997486e7fd03b46635221
sasubudh <sasubudh@SASUBUDH-87D80.partnet.cisco.com>
2018-03-16 11:22:52 +0530

    minor changes

--- SpringHibernate/src/com/hibernate/onetoone/model/Transactions.java
+++ SpringHibernate/src/com/hibernate/onetoone/model/Transactions.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
